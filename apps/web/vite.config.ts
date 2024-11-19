2013e878b767cd34d96e00874002974abccf0f55
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-18 19:06:32 -0700

    use env variable inside vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,29 +1,33 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@web": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server/src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8150",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@web": path.resolve(__dirname, "./src"),
+        "@server": path.resolve(__dirname, "../server/src"),
       },
     },
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./test/setup.ts",
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: "./test/setup.ts",
+    },
+  };
 });
