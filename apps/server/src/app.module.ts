c548ded6151bba0170a8153baeeb06914c94ab03
Blake Netzeband <blake.netzeband@gmail.com>
2024-11-18 19:46:42 -0700

    refactor to static storage

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,16 +2,26 @@ import { HttpModule } from '@nestjs/axios';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientService } from './client/client.service';
 import { ForecastController } from './forecast/forecast.controller';
 import { ForecastService } from './forecast/forecast.service';
 import { LocationController } from './location/location.controller';
 import { LocationService } from './location/location.service';
-import { ClientService } from './client/client.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CacheModule.register(), HttpModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register(),
+    HttpModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'web', 'dist'),
+      exclude: ['/api/(.*)'],
+    }),
+  ],
   controllers: [AppController, LocationController, ForecastController],
   providers: [AppService, LocationService, ForecastService, ClientService],
 })
